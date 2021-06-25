173add4c70e0a370cc76dba46e3b5e1f4e4348bc
AhmedAbdel-Moniem <ahmed.moname@gmail.com>
2021-06-25 02:23:45 +0200

    bubble sort implementation

---
+++ bubble_sort.py
@@ -0,0 +1,12 @@
+def bubble_sort(arr):
+	sorted = False
+	while not sorted:
+		sorted = True
+		for i in range(len(arr)-1):
+			if arr[i] > arr[i+1]:
+				arr[i], arr[i+1] = arr[i+1], arr[i]
+				sorted = False
+	return arr
+
+
+print(bubble_sort([5,4,3,2,1]))
