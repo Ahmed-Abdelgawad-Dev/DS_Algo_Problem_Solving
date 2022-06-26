394611116e6034a98f0b8c42672720ce6bfb1930
Ahmed-Abdelgawad-Dev <ahmed.abdelgawad.dev@gmail.com>
2022-06-26 08:54:21 +0200

    Buy a shovel - Code Forces - Less code

--- Problem_Solving/Byu a shovel - Code Forces.py
+++ Problem_Solving/Byu a shovel - Code Forces.py
@@ -2,9 +2,7 @@
 n = input().split()
 answer = 1
 while True:
-    if (int(n[0]) * answer) % 10 == 0:
-        break
-    if ((int(n[0]) * answer) % 10) - int(n[1]) == 0:
+    if ((int(n[0]) * answer) % 10 == 0) or (((int(n[0]) * answer) % 10) - int(n[1]) == 0):
         break
     answer += 1
 print(answer)
