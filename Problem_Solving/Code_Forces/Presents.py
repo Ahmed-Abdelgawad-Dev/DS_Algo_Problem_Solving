4d8281d6ef669559d07c070f216581f4504e466a
ahmed-abdelgawad-dev <ahmed.abdelgawad.dev@gmail.com>
2022-08-29 07:56:51 +0200

    A. Present - CodeForces

--- Problem_Solving/Code_Forces/Presents.py
+++ Problem_Solving/Code_Forces/Presents.py
@@ -1,2 +1,17 @@
-"""Presents"""
+"""
+A. Presents
+
+4
+# 1 2 3 4
+  2 3 4 1   
+-------
+  4 1 2 3
+"""
 n = int(input())
+friends = [int(x) for x in input().split()]
+obj = {}
+for i in range(n):
+    obj[friends[i]] = i+1
+
+for key in sorted(obj.keys()):
+    print(obj[key], end=' ')
