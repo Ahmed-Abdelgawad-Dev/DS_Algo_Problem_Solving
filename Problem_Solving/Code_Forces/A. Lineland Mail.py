d2176ce134d091cb7d37317379493fe9bf7537b2
ahmed-abdelgawad-dev <ahmed.abdelgawad.dev@gmail.com>
2022-09-09 03:48:54 +0200

    A line land - code forces

--- Problem_Solving/Code_Forces/A. Lineland Mail.py
+++ Problem_Solving/Code_Forces/A. Lineland Mail.py
@@ -1,6 +1,18 @@
-
-n=int(input());
-x=list(map(int,input().split()))
+n = int(input())
+cities = list(map(int, input().split()))
+result = []
 for i in range(n):
-    t=x[i];
-    print(min(abs(t-x[i-1]),abs(x[i-n+1]-t)),max(t-x[0],x[n-1]-t))
+	# Maximum value in all cases except 2 cases ({first & last} elements of array)
+	maxi = max(abs(cities[i] - cities[0]), abs(cities[i] - cities[-1]))
+	# Exception One -> i==First index of array
+	if i == 0:
+		mini = abs(cities[i] - cities[1])
+	# Exception Two -> i==last index of array
+	if i == n - 1:
+		mini = abs(cities[i] - cities[-2])
+	else:
+		# Minimum value in all cases except 2 cases ({first & last} elements of array)
+		mini = min(abs(cities[i] - cities[i - 1]), abs(cities[i] - cities[i + 1]))
+	result.append([mini, maxi])
+for x in result:
+	print(*x)
