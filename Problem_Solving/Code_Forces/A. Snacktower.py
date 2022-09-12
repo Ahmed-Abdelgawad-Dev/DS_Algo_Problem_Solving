a5e88094226602e933278ccc447cd247e5a4f33e
ahmed-abdelgawad-dev <ahmed.abdelgawad.dev@gmail.com>
2022-09-12 09:34:17 +0200

    snack tower code forces

---
+++ Problem_Solving/Code_Forces/A. Snacktower.py
@@ -0,0 +1,16 @@
+n = int(input())
+sizes = list(map(str, input().split()))
+
+que = []
+for i in range(len(sizes)):
+	if sizes[i] == max(sizes[i:]):
+		if len(que) >= 1:
+			if sizes[i] not in que:
+				que.append(sizes[i])
+				print(*que[::-1], sep=' ')
+				que.clear()
+		else:
+			print(sizes[i])
+	else:
+		que.append(sizes[i])
+		print()
