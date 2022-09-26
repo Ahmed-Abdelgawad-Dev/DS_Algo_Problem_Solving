d195b8b0ad5b4f511179380c0a6b1a8e7736bf1f
ahmed-abdelgawad-dev <ahmed.abdelgawad.dev@gmail.com>
2022-09-26 21:14:23 +0400

    A.Twin

--- Problem_Solving/Code_Forces/main.py
+++ Problem_Solving/Code_Forces/main.py
@@ -1,3 +1,9 @@
-int(input())
-letters = list(set(list(input().lower())))
-print('YES') if len(letters) == 26 else print('NO')
+b = int(input())
+a = list(map(int, input().split()))
+a.sort(reverse=True)
+c = 0
+for i in range(b):
+	c += a[i]
+	if c > sum(a) / 2:
+		print(i + 1)
+		break
