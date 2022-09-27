eb4879bc296f1801eb6fe0e9c32d168a9b410ff3
ahmed-abdelgawad-dev <ahmed.abdelgawad.dev@gmail.com>
2022-09-27 12:47:13 +0400

    A.Keyboard customization

--- Problem_Solving/Code_Forces/A keyboard Code Forces.py
+++ Problem_Solving/Code_Forces/A keyboard Code Forces.py
@@ -1,12 +1,9 @@
 s = "qwertyuiopasdfghjkl;zxcvbnm,./"
-string = [i for i in s]
-direction = input().upper()       #"R"
-_input = input()                  #"s;;upimrrfod;pbr"
+s = [i for i in s]
+direction = input().upper()
+_input = input()
 for i in _input:
-	if i in string:
-		if direction == 'R':
-			pos = string.index(i) - 1
-			print(string[pos], end='')
-		if direction == 'L':
-			pos = string.index(i) + 1
-			print(string[pos], end='')
+	if direction == 'R':
+		print(s[s.index(i) - 1], end='')
+	if direction == 'L':
+		print(s[s.index(i) + 1], end='')
